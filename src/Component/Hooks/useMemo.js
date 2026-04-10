6f72b4ed0c038ff2139910f3f53f942875cc3f7e
Namrata Sahani <namratasahani@Nmaratas-MacBook.local>
2026-04-10 23:36:45 +0530

    minor code upadted

--- src/Component/Hooks/useMemo.js
+++ src/Component/Hooks/useMemo.js
@@ -5,7 +5,7 @@ const Calculation = () => {
     const [num, setNum] = useState(5);
 
     const expensiveCalculation = (num) => {
-        console.log("Clculating ......");
+        console.log("Clculating .....");
         let result = 0
         for (let i = 0; i < 1000; i++) {
             result = result + num * 2
